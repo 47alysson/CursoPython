59f28db2ade4d1197df069ca35d3a1b701796258
AlyssonOliveira <alysson.oliveira@contabilivre.com.br>
2024-12-18 17:10:59 -0300

    muito sono

--- pacote-download/Intermediário/aula82.py
+++ pacote-download/Intermediário/aula82.py
@@ -1,3 +1,32 @@
 def executa (funcao, *args):
     return funcao(*args)
 
+def soma(x,y):
+    return x + y
+
+def cria_multiplicador(multiplicador):
+    def multiplica(numero):
+        return numero * multiplicador
+    return multiplica
+
+print(
+    executa(lambda x, y: x + y, 2, 4)
+)
+
+print (
+    executa(lambda multiplicador,numero: numero*multiplicador,3, 4)
+)
+
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2
+)
+
+print (duplica(4))
+
+print(
+    executa(
+        lambda *args: sum(args),
+        1,3,5,6
+    )
+)
