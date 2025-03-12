be11f17e753e4be62ceeda96940e4009b6f517e2
AlyssonOliveira <alysson.oliveira@contabilivre.com.br>
2025-03-12 13:33:44 -0300

    try except

--- pacote-download/Intermediário/aula93.py
+++ pacote-download/Intermediário/aula93.py
@@ -4,15 +4,18 @@ try:
     a = 18
     b = 0
     #print(b[0])
-    print('Linha 1'[1000])
+    #print('Linha 1'[1000])
     c = a / b    
     print('Linha 2')
-except ZeroDivisionError:
+except ZeroDivisionError as e:
     print('Dividiu por zero')
+    print(e.__class__.__name__)
 except NameError:
     print('Erro de nome')
-except (TypeError,IndexError):
+except (TypeError,IndexError) as error:
     print('TypeError + IndexError')
+    print('MSG:',error)
+    print('Nome:',error.__class__.__name__)
 except Exception:
     print('Erro desconhecido')
 
