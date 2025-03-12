a3ccd1fde861a7bae64587587566ff79885e0568
AlyssonOliveira <alysson.oliveira@contabilivre.com.br>
2025-03-12 17:11:04 -0300

    raise, módulos, packages

--- pacote-download/Intermediário/aula94.py
+++ pacote-download/Intermediário/aula94.py
@@ -1,2 +1,14 @@
 #try, except, else e finaly
 
+try:
+    print('Abrir arquivo')
+    #8/0
+
+except ZeroDivisionError:
+    print('Dividiu zero')
+
+else:
+    print('Não deu erro')
+finally:
+    print('Fechar arquivo')
+    
---
+++ pacote-download/Intermediário/aula99_package/modulo.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'variavel',
+    'soma_do_modulo',
+    'nova'
+]
+
+variavel = 'Algo'
+
+def soma_do_modulo (x,y):
+    return x+y
+
+nova = 0
